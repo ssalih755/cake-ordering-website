fd29383745cdaebca656f97c9bbfc397eedb1b92
Sarrah Salih <sarrahsalih2@gmail.com>
2025-04-18 14:45:53 -0400

    separated MainNav styling from global into module

--- capstone/react/src/components/MainNav/MainNav.jsx
+++ capstone/react/src/components/MainNav/MainNav.jsx
@@ -2,34 +2,33 @@ import { Link, NavLink } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
 import cakeLogo from "./cakeLogo.png";
+import styles from "./MainNav.module.css";
 
 export default function MainNav() {
   const user = useContext(UserContext);
 
   return (
-    <div className="nav-box">
-       <img src={cakeLogo} alt="logo" id="logo" />
-    <nav id="main-nav" className="nav-list">
-       
-     
-      <div className="nav-link">
-        <NavLink to="/">Home</NavLink>
-      </div>
-      {user ? (
-        <>
-          <div   className="nav-link">
-            <NavLink to="/userProfile">Profile</NavLink>
-          </div>
-          <div className="nav-link">
-            <Link to="/logout">Logout</Link>
-          </div>
-        </>
-      ) : (
-        <div className="nav-link">
-          <NavLink to="/login">Login</NavLink>
+    <div className={styles.navBox} id="nav-box">
+      <img src={cakeLogo} alt="logo" className={styles.logo} />
+      <nav id="main-nav" className={styles.navList}>
+        <div className={styles.navLink}>
+          <NavLink to="/">Home</NavLink>
         </div>
-      )}
-    </nav>
+        {user ? (
+          <>
+            <div className={styles.navLink}>
+              <NavLink to="/userProfile">Profile</NavLink>
+            </div>
+            <div className={styles.navLink}>
+              <Link to="/logout">Logout</Link>
+            </div>
+          </>
+        ) : (
+          <div className={styles.navLink}>
+            <NavLink to="/login">Login</NavLink>
+          </div>
+        )}
+      </nav>
     </div>
   );
 }
