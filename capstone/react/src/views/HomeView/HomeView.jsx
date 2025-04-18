0aef0ba960f82dedfd876577f2ceafc5a6f4e29d
Melissa <missyharper44133@gmail.com>
2025-04-18 14:08:39 -0400

    stuff

---
+++ capstone/react/src/views/HomeView/HomeView.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import AuthService from "../../services/AuthService";
+import Notification from "../../components/Notification/Notification";
+import axios from "axios";
+
+import styles from "./LoginView.module.css";
+
+export default function HomeView() {
+  return (
+    <div>HomeView</div>
+  )
+}
