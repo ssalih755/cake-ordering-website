06c23a52ab554e86d8221655e1be90ad6343b0c0
Sarrah Salih <sarrahsalih2@gmail.com>
2025-04-18 09:27:24 -0400

    some styling for Login page and added header footer folders

--- capstone/react/src/views/LoginView/LoginView.jsx
+++ capstone/react/src/views/LoginView/LoginView.jsx
@@ -1,22 +1,19 @@
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import AuthService from '../../services/AuthService';
-import Notification from '../../components/Notification/Notification';
-import axios from 'axios';
+import AuthService from "../../services/AuthService";
+import Notification from "../../components/Notification/Notification";
+import axios from "axios";
 
-import styles from './LoginView.module.css';
+import styles from "./LoginView.module.css";
 
 export default function LoginView({ onLogin }) {
-
   const navigate = useNavigate();
   const [notification, setNotification] = useState(null);
 
   // Setup state for the registration data
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -26,51 +23,66 @@ export default function LoginView({ onLogin }) {
         // Grab the user and token
         const user = response.data.user;
         const token = response.data.token;
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
         // Add the login data to local storage
-        localStorage.setItem('user', JSON.stringify(user));
-        localStorage.setItem('token', token);
+        localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem("token", token);
 
         // Use the callback to add user to state
         onLogin(user);
 
         // Navigate to the home page
-        navigate('/');
+        navigate("/");
       })
       .catch((error) => {
         // Check for a response message, but display a default if that doesn't exist
-        const message = error.response?.data?.message || 'Login failed.';
-        setNotification({ type: 'error', message: message });
+        const message = error.response?.data?.message || "Login failed.";
+        setNotification({ type: "error", message: message });
       });
   }
-  
+
   return (
+    <div id="view-login" className={styles.viewLogin}>
+      <h2>Login</h2>
+      <p className={styles.registerText}>
+        Not registered? Create an account <Link to="/register"> here!</Link>
+      </p>
+      <Notification
+        notification={notification}
+        clearNotification={() => setNotification(null)}
+      />
 
-    <div id="view-login" className={styles.viewLogin} >
-      
-
-      <h2>Login</h2> 
-      
-
-      <Notification notification={notification} clearNotification={() => setNotification(null)} />
-
-      <form onSubmit={handleSubmit}>
-
+      <form className={styles.loginForm} onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="username">Username:</label>
-          <input type="text" id="username" value={username} size="50" required autoFocus autoComplete="username"
-              onChange={ event => setUsername(event.target.value)} />
+          <input
+            type="text"
+            id="username"
+            value={username}
+            size="50"
+            required
+            autoFocus
+            autoComplete="username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
         </div>
 
         <div className="form-control">
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" value={password} size="50" required
-              onChange={ event => setPassword(event.target.value)} />
+          <input
+            type="password"
+            id="password"
+            value={password}
+            size="50"
+            required
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </div>
 
-        <button type="submit" className={`btn-primary ${styles.formButton}`}>Sign in</button>
-        <Link to="/register">New? Register here!</Link>
+        <button type="submit" className={`btn-primary ${styles.formButton}`}>
+          Sign in
+        </button>
       </form>
     </div>
   );
