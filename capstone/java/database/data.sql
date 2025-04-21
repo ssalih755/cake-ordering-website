59aec7fa2aff50bbec81bac46321ef3edebedb7e
Sarrah Salih <sarrahsalih2@gmail.com>
2025-04-21 16:00:47 -0400

    Registration page completed

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -3,5 +3,7 @@ BEGIN TRANSACTION;
 -- the password for both users is "password"
 INSERT INTO users (username,password_hash,role, firstname, lastname, email, phone) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', 'user', 'user', 'user@gmail.com', '3303303303');
 INSERT INTO users (username,password_hash,role, firstname, lastname, email, phone) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', 'admin', 'admin', 'admin@gmail.com','2622622622');
+INSERT INTO users (username,password_hash,role, firstname, lastname, email, phone) VALUES ('missy','$2y$10$gMwCv472n6HGgMgc4RqnieWV9fuydw2n4SY4gsvnvwI8NHYYBgqcG','ROLE_ADMIN', 'missy', 'harper', 'missy@gmail.com','2622622622');
+INSERT INTO users (username,password_hash,role, firstname, lastname, email, phone) VALUES ('sarrah','$2y$10$gMwCv472n6HGgMgc4RqnieWV9fuydw2n4SY4gsvnvwI8NHYYBgqcG','ROLE_USER', 'sarrah', 'salih', 'sarrah@email.com','5556667778');
 
 COMMIT TRANSACTION;
--- capstone/react/src/views/RegisterView/RegisterView.jsx
+++ capstone/react/src/views/RegisterView/RegisterView.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import AuthService from '../../services/AuthService';
-import Notification from '../../components/Notification/Notification';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import AuthService from "../../services/AuthService";
+import Notification from "../../components/Notification/Notification";
 
-import styles from './RegisterView.module.css';
+import styles from "./RegisterView.module.css";
 
 export default function RegisterView() {
   const navigate = useNavigate();
@@ -11,46 +11,63 @@ export default function RegisterView() {
   const [notification, setNotification] = useState(null);
 
   // Setup state for the registration data
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
 
-
     // Validate the form data
     if (password !== confirmPassword) {
       // Passwords don't match, so display error notification
-      setNotification({ type: 'error', message: 'Passwords do not match.' });
+      setNotification({ type: "error", message: "Passwords do not match." });
     } else {
       // If no errors, send data to server
       AuthService.register({
         username,
         password,
         confirmPassword,
-        role: 'user',
+        role: "user",
+        firstname,
+        lastname,
+        email,
+        phone,
       })
         .then(() => {
-          setNotification({ type: 'success', message: 'Registration successful' });
-          navigate('/login');
+          setNotification({
+            type: "success",
+            message: "Registration successful",
+          });
+
+          navigate("/login");
         })
         .catch((error) => {
           // Check for a response message, but display a default if that doesn't exist
-          const message = error.response?.data?.message || 'Registration failed.';
-          setNotification({ type: 'error', message: message });
+          const message =
+            error.response?.data?.message || "Registration failed.";
+          setNotification({ type: "error", message: message });
         });
     }
   }
 
   return (
-    <div id="view-register">
+    <div className={styles.viewRegister} id="view-register">
       <h2>Register</h2>
-
-      <Notification notification={notification} clearNotification={() => setNotification(null)} />
+      <div className={styles.loginText}>
+        <Link to="/login">Have an account? Log-in</Link>
+      </div>
+      <Notification
+        notification={notification}
+        clearNotification={() => setNotification(null)}
+      />
 
       <form onSubmit={handleSubmit}>
-        <div className="form-control">
+        <div className={styles.formControl}>
           <label htmlFor="username">Username:</label>
           <input
             type="text"
@@ -64,7 +81,7 @@ export default function RegisterView() {
           />
         </div>
 
-        <div className="form-control">
+        <div className={styles.formControl}>
           <label htmlFor="password">Password:</label>
           <input
             type="password"
@@ -76,7 +93,7 @@ export default function RegisterView() {
           />
         </div>
 
-        <div className="form-control">
+        <div className={styles.formControl}>
           <label htmlFor="confirmPassword">Confirm Password:</label>
           <input
             type="password"
@@ -88,10 +105,56 @@ export default function RegisterView() {
           />
         </div>
 
+        <div className={styles.formControl}>
+          <label htmlFor="First Name">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            value={firstname}
+            size="50"
+            required
+            onChange={(event) => setFirstName(event.target.value)}
+          />
+        </div>
+        <div className={styles.formControl}>
+          <label htmlFor="Last Name">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            value={lastname}
+            size="50"
+            required
+            onChange={(event) => setLastName(event.target.value)}
+          />
+        </div>
+
+        <div className={styles.formControl}>
+          <label htmlFor="Email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            size="50"
+            required
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+
+        <div className={styles.formControl}>
+          <label htmlFor="Phone">Phone:</label>
+          <input
+            type="text"
+            id="phone"
+            value={phone}
+            size="50"
+            required
+            onChange={(event) => setPhone(event.target.value)}
+          />
+        </div>
+
         <button type="submit" className={`btn-primary ${styles.formButton}`}>
           Register
         </button>
-        <Link to="/login">Have an account? Log-in</Link>
       </form>
     </div>
   );
