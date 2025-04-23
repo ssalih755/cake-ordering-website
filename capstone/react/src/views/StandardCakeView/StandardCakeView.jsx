57a50cff96aef70e679c93c9f3a6e0e3f40d1a24
Ben Friend <bensaalfriend@yahoo.com>
2025-04-23 17:34:31 -0400

    button styling complete

--- capstone/react/src/views/StandardCakeView/StandardCakeView.jsx
+++ capstone/react/src/views/StandardCakeView/StandardCakeView.jsx
@@ -1,22 +1,16 @@
-import React, { useEffect, useState,useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./StandardCakeView.module.css";
-import cake from "../HomeView/cake.png";
-import cake1 from "../HomeView/cake1.png";
-import cake2 from "../HomeView/cake2.png";
-import cake3 from "../HomeView/cake3.png";
 import CakeService from "../../services/CakeService";
 import CakeCard from "../../components/CakeCard/CakeCard";
 import { UserContext } from "../../context/UserContext";
-
-// import { UserContext } from "../../path-to-your-UserContext"; // Update this path
 import { isAdmin } from "../../services/UserHelper"; // Update this path
 
 export default function StandardCakeView() {
   const [cakes, setCakes] = useState([]);
   const user = useContext(UserContext);
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     CakeService.getStandardCakes()
       .then((response) => {
@@ -26,24 +20,22 @@ export default function StandardCakeView() {
         console.log(err);
       });
   }, []);
-  
+
   const handleAddNewCake = () => {
-    
-    navigate("/admin/add-cake");      //this needs to have its path updated
+    navigate("/addcake");
   };
-  
+
   return (
     <div className={styles.pageContainer}>
       <div className={styles.headerContainer}>
+       <div className={styles.titleAndButtonContainer}>
         <h1>Standard Cakes</h1>
         {isAdmin(user) && (
-          <button 
-            className={styles.adminButton}
-            onClick={handleAddNewCake}
-          >
-            Add New Cake
+          <button className={styles.adminButton} onClick={handleAddNewCake}>
+            Add Cake
           </button>
         )}
+        </div>
       </div>
       <div className={styles.cardContainer}>
         {cakes.map((cake) => {
