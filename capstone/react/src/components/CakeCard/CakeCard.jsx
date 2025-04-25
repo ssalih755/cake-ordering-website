60b8c20977e741b5422b5e545203d34ebbd8c089
Melissa <missyharper44133@gmail.com>
2025-04-25 15:34:13 -0400

    added navigation to the standard cake to buy now product page

--- capstone/react/src/components/CakeCard/CakeCard.jsx
+++ capstone/react/src/components/CakeCard/CakeCard.jsx
@@ -3,18 +3,23 @@ import cakePic from "../../Views/HomeView/cake.png";
 import styles from "./CakeCard.module.css";
 import { isAdmin } from "../../services/UserHelper";
 import { UserContext } from "../../context/UserContext";
-
+import { useNavigate } from "react-router-dom"; 
 export default function CakeCard({ cake, onAvailabilityChanged }) {
   const user = useContext(UserContext);
-
+  const navigate = useNavigate();
+  
+  const handleStandardCake = () => { navigate(`/cakes/${cake.id}`); };
   return (
-    <div className={styles.card}>
+    <div className={styles.card}
+    onClick={handleStandardCake}
+      style={{ cursor: "pointer" }}>
       <img src={cake.imgURL || cakePic} alt="Bams Cakery" />
       <div className="card-body">
+    
         <h5 className="card-name">{cake.name}</h5>
         <p className="card-text">{cake.description}</p>
         <h2>${cake.price}</h2>
-
+    
         <section className={styles.buttonContainer}>
           {isAdmin(user) && (
             <button
